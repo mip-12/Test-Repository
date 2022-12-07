e925e877e826d584988b90135465d0726da8f705
mip-12 <mip.globaliasoft@gmail.com>
2022-12-07 11:30:31 +0530

    done some changes

--- home_automation/user/views.py
+++ home_automation/user/views.py
@@ -78,14 +78,14 @@ class UserLoginView(APIView):
     def post(self, request, format=None):
 
 
-        from django.core.mail import EmailMultiAlternatives
+        # from django.core.mail import EmailMultiAlternatives
 
-        subject, from_email, to = 'hello', 'pminesh470@gmail.com', 'mip.globaliasoft@gmail.com'
-        text_content = 'This is an important message.'
-        html_content = '<p>This is an <strong>important</strong> message.</p>'
-        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-        msg.attach_alternative(html_content, "text/html")
-        msg.send()
+        # subject, from_email, to = 'hello', 'pminesh470@gmail.com', 'mip.globaliasoft@gmail.com'
+        # text_content = 'This is an important message.'
+        # html_content = '<p>This is an <strong>important</strong> message.</p>'
+        # msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        # msg.attach_alternative(html_content, "text/html")
+        # msg.send()
 
 
 
